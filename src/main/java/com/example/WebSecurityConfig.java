aaac743ed778db7eca97d9845a5f6ae887084e9e
Adzkia Husnul Abidat <adzkiahusnulabidat@gmail.com>
2017-11-18 14:05:14 +0700

    fourth commit

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
@@ -39,4 +45,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser("admin").password("admin")
 		.roles("ADMIN");
 	}
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery("select username,password, enabled from users where username=?")
+			.authoritiesByUsernameQuery("select username,role from user_roles where username=?");
+	}
+	
+	
 }
