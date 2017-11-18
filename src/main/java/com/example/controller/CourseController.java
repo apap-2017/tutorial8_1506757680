a8df278728e6be9768c6db8ec75934e6018803e0
Adzkia Husnul Abidat <adzkiahusnulabidat@gmail.com>
2017-11-18 07:54:59 +0700

    first commit

---
+++ src/main/java/com/example/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.model.CourseModel;
+import com.example.service.CourseService;
+
+@Controller
+public class CourseController {
+	@Autowired
+	CourseService courseDAO;
+	
+    @RequestMapping("/course/view/{id}")
+    public String selectCourse (Model model,
+            @PathVariable(value = "id") String id)
+    {
+        CourseModel course = courseDAO.selectCourse(id);
+
+        if (course != null) {
+            model.addAttribute ("course", course);
+            return "viewcourses";
+        } else {
+            model.addAttribute ("id", id);
+            return "nocourse";
+        }
+    }
+}
---
+++ src/main/java/com/example/dao/CourseMapper.java
@@ -0,0 +1,31 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Many;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+@Mapper
+public interface CourseMapper {
+	@Select("select id_course, name, credits from course where id_course = #{id_course}")
+  @Results(value = {
+  	@Result(property="id_course", column="id_course"),
+  	@Result(property="name", column="name"),
+  	@Result(property="credits", column="credits"),
+  	@Result(property="students", column="id_course",
+  	javaType = List.class,
+  	many = @Many(select = "selectParticipants"))
+  })
+  CourseModel selectCourse(@Param("id_course") String id_course);
+  
+  @Select("select student.name, studentcourse.npm " + "from studentcourse join student " + 
+  "on studentcourse.npm = student.npm " + "where studentcourse.id_course = #{id_course}")
+  List <StudentModel> selectParticipants (@Param("id_course") String id_course);
+}
